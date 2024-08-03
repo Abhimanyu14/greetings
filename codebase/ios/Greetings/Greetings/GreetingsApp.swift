e06a8d8c62476cda02d3f8eb247090d2ad1484e5
Abhimanyu14 <abhimanyu.n14@gmail.com>
2024-08-03 11:06:19 +0530

    [TECH] project created

---
+++ codebase/ios/Greetings/Greetings/GreetingsApp.swift
@@ -0,0 +1,17 @@
+//
+//  GreetingsApp.swift
+//  Greetings
+//
+//  Created by Abhimanyu on 03/08/24.
+//
+
+import SwiftUI
+
+@main
+struct GreetingsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
